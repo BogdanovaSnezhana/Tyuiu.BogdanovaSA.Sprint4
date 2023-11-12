79f85f2b5ea58c894d639c8d32fdd563b5037d09
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-11-12 15:14:49 +0500

    Task5

---
+++ Tyuiu.BogdanovaSA.Sprint4.Task5.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BogdanovaSA.Sprint4.Task5.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BogdanovaSA.Sprint4.Task5.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cf856fe-20fe-477a-b4aa-369faec1049b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
