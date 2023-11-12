5bfa7eb34106ee156a2b485577f39ea88c81efd0
tyuiu.bogdanovasnezhana@outlook.com <bogdanova.snezha09@gmail.com>
2023-11-12 23:55:57 +0500

    Task7

---
+++ Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Lib;
+
+namespace Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+        {
+            [TestMethod]
+            public void ValidCalc()
+            {
+                DataService ds = new DataService();
+
+                int rows = 4;
+                int columns = 2;
+                int[,] mtrx = new int[rows, columns];
+                string str = "56789012";
+                int res = ds.Calculate(rows, columns, str);
+                int wait = 4;
+                Assert.AreEqual(wait, res);
+            }
+
+        } 
+}
---
+++ Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BogdanovaSA.Sprint4.Task7.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d094411-a01c-4e03-be0f-3179fa333062")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
